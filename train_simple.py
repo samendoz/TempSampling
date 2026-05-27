625d3b31abb2bffed90da88e3c6bae9e431c20da
samendoz <samendoz@espol.edu.ec>
2026-05-27 13:16:34 -0600

    test: modifying profiling for to_gdl_blocks

--- train_simple.py
+++ train_simple.py
@@ -941,7 +941,9 @@ for e in range(train_param['epoch']):
             print_to_dgl_blocks_profile()
             tmp_to_dgl_blocks_profile = get_to_dgl_blocks_profile_summary()
             print(tmp_to_dgl_blocks_profile)
+            print("\t Captured to_dgl_blocks time: {:.2f}%".format(100 * tmp_to_dgl_blocks_profile['total_time'] / estimated_prep_times["initial_to_dgl_blocks"] if estimated_prep_times["initial_to_dgl_blocks"] > 0 else 0))
             reset_to_dgl_blocks_profile()
+
 
 
         # batch_sizes.append(batch_count)
